31d3f1bcad3b267149e5c04203b0b5adc215e8f3
TobiasDalton <tjd052799@gmail.com>
2024-06-25 01:36:54 -0400

    (0.319) there is another... location card string

--- js/code.js
+++ js/code.js
@@ -34,7 +34,7 @@ const phraseData = [
     [">{type}","Pokemon with moves effective against {type}","Will return Pokemon with at least one attack dealing super effective damage to {type}<br><a href='https://i.imgur.com/3Qyr3x8.png'>BUG</a>: Ignores 2nd charge move.",">Fire"],
     ["pokemon - keywords","Pokemon which are...","various qualifiers certain pokemon meet",,true],
     ["costume", "wearing <a href='https://pokemongo.fandom.com/wiki/Event_Pokémon'>clothing</a>","Caught during certain events."],
-    ["location<br>card","having a special catch card","Caught during certain <a href='https://pokemongo.fandom.com/wiki/Location_Cards'>in-person events</a>","'locationbackground'"],
+    ["{location_card}","having a special catch card","Options:<br>'locationbackground': Caught during certain <a href='https://pokemongo.fandom.com/wiki/Location_Cards'>in-person events</a><br>'specialbackground': ???. Coming soon...<br>'background': Both location and ??? cards"],
     ["eggsonly", "<a href='https://bulbapedia.bulbagarden.net/wiki/Baby_Pokémon'>Baby</a> species", "Typically only found in eggs"],
     ["{keyword}","{keyword}", "Options: legendary, mythical, 'ultra beasts', shadow, purified, shiny, lucky."],
     ["defender","currently defending a gym"],
